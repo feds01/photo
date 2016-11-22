ada44fcda87035de51e833744d27f7a9f5dc305f
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2016-11-22 19:50:07 +0000

    started work on black_list

--- src/blacklist_hook.py
+++ src/blacklist_hook.py
@@ -1,10 +1,27 @@
 from src.utils import *
 
 class Blacklist:
-    def __init__(self, directory):
+    def __init__(self, directory, *args):
+        self.black_list_type = list(*args)
         self.directory = directory
         self.black_list = []
+        self.file_location = Directory(__file__).get_artifact_file_location("blacklist.txt")
+        self.black_list_instance = {}
+        self.symbol = ""
 
-    def checkDirectory(self):
-        pass
+    def package_directory(self):
+        self.black_list_instance = {}
+        self.black_list_instance.update({self.directory: self.black_list_type})
+
+
+    def create_instance(self):
+        self.package_directory()
+        with open(self.file_location, "r") as file:
+            if not file.readlines():
+                pass
+            else:
+                self.symbol = "\n"
+            file.close()
+        with open(self.file_location, "a") as file:
+            file.write(self.symbol + str(self.black_list_instance)), file.close()
 
