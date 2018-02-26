6ae9b799b424156114ddf28a7f98767bd7d2e0d3
Alex <alexander.fedotov.uk@gmail.com>
2018-02-26 23:05:03 +0000

    refined global_get() function to use regex rather than hard-coded rules

--- src/utilities/shorts.py
+++ src/utilities/shorts.py
@@ -1,4 +1,7 @@
 import os
+
+import re
+
 from src.core.exceptions import *
 from src.utilities.arrays import center_array
 
@@ -23,7 +26,7 @@ def shorten(path, max_len=30, sep_char=False, separator=os.sep):
     if overflow <= 0:
         return path
     # begin processing path
-    #print(path)
+    # print(path)
     chunks = path.split(separator)
     usable_chunks = chunks[2:-1]  # parts of the directory which can be used to shorten the overall path
 
@@ -34,22 +37,22 @@ def shorten(path, max_len=30, sep_char=False, separator=os.sep):
 
         # initial selection for candidates to be removed
         for position in centered_list:
-            #print(overflow_size, position)
+            # print(overflow_size, position)
             if overflow_size > 0:
                 replace_chunks.append(position)
                 overflow_size -= len(position)
             elif overflow_size <= 0:
                 break
-        #print(replace_chunks)
+        # print(replace_chunks)
         if overflow_size > 0:
             pass
-        #print(replace_chunks)
+        # print(replace_chunks)
         for chunk in replace_chunks:
             if not chunk_replacer in chunks:
                 chunks.insert(chunks.index(chunk), chunk_replacer)
             chunks.remove(chunk)
 
-    #print('centered: %s\n useable: %s\n chunks: %s' % (centered_list, usable_chunks, chunks))
+    # print('centered: %s\n useable: %s\n chunks: %s' % (centered_list, usable_chunks, chunks))
     if len(usable_chunks) < 1:
         usable_chunks = chunks[1:-1]
 
@@ -64,68 +67,28 @@ def shorten(path, max_len=30, sep_char=False, separator=os.sep):
     return separator.join(chunks)
 
 
-def global_get(data_structure, req):
-    keys = []
-    pos = 0
-    found_dot = False
-    while True:
-        if pos > len(req) - 1:
-            break
-        else:
-            symbol = req[pos]
+def global_get(data, req):
+    keys = req.split('.')
+    quote_keys = re.compile(r"\.\'([^\']*)\'").findall(req)
 
-        if req[0] == "'" and not found_dot:
-            pos = (req[1:]).index("'")
-            keys.append(req[1: pos + 1])
-            found_dot = True
+    # parse and fix-up the req
+    for pos, key in enumerate(keys):
+        if key == "'":
+            keys.pop(pos)
+            keys[pos] = quote_keys[0]
 
-        if symbol == '.':
-            if not found_dot:
-                keys.append(req[0: pos])
-                found_dot = True
+            quote_keys.pop(0)
 
-            if req[pos + 1] == "'":
-                pos += 2
-                o_pos = pos
-                symbol = ''
-                while symbol != "'":
-                    if pos > len(req) - 1:
-                        keys.append(req[o_pos: pos])
-                        break
-                    else:
-                        symbol = req[pos]
-                        if symbol == "'":
-                            keys.append(req[o_pos: pos])
-                    pos += 1
-
-            else:
-                symbol = ''
-                pos += 1
-                o_pos = pos
-                while symbol != '.':
-                    if pos > len(req) - 1:
-                        keys.append(req[o_pos: pos])
-                        break
-                    else:
-                        symbol = req[pos]
-                        if symbol == '.':
-                            keys.append(req[o_pos: pos])
-                    pos += 1
-                pos -= 1
-        else:
-            pos += 1
-
-    if len(keys) == 0:
-        keys.append(req)
-
+    # extract data from the dict now
     for key in keys:
-        if key in data_structure.keys():
-            data_structure = data_structure.get(key)
-            continue
+        if key in data.keys():
+            data = data.get(key)
         else:
-            raise Fatal('Unreadable information', 'key was not found, but expected', 'key=%s' % req, 'given_data=%s' % data_structure)
-    try:
-        return data_structure.keys()
+            raise Fatal('Unreadable information', 'key was not found, but expected',
+                        'key=%s' % req, 'given_data=%s' % data)
 
+    # try first return keys, if non-existent just return data
+    try:
+        return data.keys()
     except AttributeError:
-        return data_structure
+        return data
